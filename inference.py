e00741cfdc57343b3276bfe9fc83ddd2bdf102e0
OlegSivets <legolas2002@mail.ru>
2023-11-24 16:14:49 +0300

    Сheck of the detection result was added

--- inference.py
+++ inference.py
@@ -3,13 +3,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-v', '--video-path', help='Path to video folder', type=str, default='./data')
-parser.add_argument('-d', '--detection-model', help='Path to YOLO detection model weights', type=str, default='models/yolo_best.pt')
+parser.add_argument('-v', '--video-path', help='Path to video folder', type=str, default='./data/3554032.mp4')
+parser.add_argument('-d', '--detection-model', help='Path to YOLO detection model weights', type=str, default='./models/yolov8l_e20_b8_im720.pt')
 
 args = parser.parse_args()
-
-H_MODEL_PATH = './models/yolov8l_e20_b8_im720.pt'
-VIDEO_PATH = './data/3554032.mp4'
 
 model = CDWnet(hard_model=args.detection_model)
 class_res, image = model.predict(args.video_path)
