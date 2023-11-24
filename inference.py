5a16daaf3b3f6c709b7aac6347aa5fcbca86002f
OlegSivets <legolas2002@mail.ru>
2023-11-24 15:44:29 +0300

    Inference was changed

--- inference.py
+++ inference.py
@@ -3,14 +3,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-# parser.add_argument('-i', '--input', help='Path to video folder', type=str, default='./data')
-# parser.add_argument('-o', '--output', help='Output csv-file', type=str, default='results.csv')
-# parser.add_argument('-d', '--detection-weights', help='Path to YOLO detection model weights', type=str, default='models/yolo_best.pt')
+parser.add_argument('-v', '--video-path', help='Path to video folder', type=str, default='./data')
+parser.add_argument('-d', '--detection-model', help='Path to YOLO detection model weights', type=str, default='models/yolo_best.pt')
 
 args = parser.parse_args()
 
 H_MODEL_PATH = './models/yolov8l_e20_b8_im720.pt'
 VIDEO_PATH = './data/3554032.mp4'
 
-model = CDWnet(hard_model=H_MODEL_PATH)
-res_class = model.predict(VIDEO_PATH)
+model = CDWnet(hard_model=args.detection_model)
+class_res, image = model.predict(args.video_path)
+print(f'CDW Class: {class_res}')
