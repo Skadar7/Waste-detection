6fc35b8aa4bdf83e9c5852b39740d7c8ddba0035
OlegSivets <legolas2002@mail.ru>
2023-11-25 04:23:19 +0300

    Test was updated

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 from model import CDWnet
 from os import listdir
 from os.path import isfile, join
+import csv
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-hd', '--hard-detection-model', help='Path to YOLO detection model weights', type=str, default='./models/yolov8l_e20_b8_im720.pt')
 parser.add_argument('-t', '--test-path', help='Path to directory with videos', type=str, default='./videos')
+parser.add_argument('-o', '--output-path', help='Save path for csv file', type=str, default='./result.csv')
 
 args = parser.parse_args()
 
@@ -13,11 +15,19 @@ model = CDWnet(hard_model=args.hard_detection_model)
 
 FILES = [f for f in listdir(args.test_path) if isfile(join(args.test_path, f))]
 
+classes = {
+    'Brick': 'кирпич',
+    'Concrete': 'бетон',
+    'priming': 'грунт',
+    'Tree': 'дерево'
+}
+
 res = []
 
 for path in FILES:
     class_res, image = model.predict(f'{args.test_path}/{path}')
-    res.append([path, class_res])
+    res.append([path, classes[class_res]])
 
-for det in res:
-    print(f'Video {det[0]}: {det[1]}')
+with open(args.output_path, 'w') as f:
+    writer = csv.writer(f, lineterminator='\n', delimiter=';')
+    writer.writerows(res)
